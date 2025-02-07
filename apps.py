e1103c43e8c1741a2575b2d3ee0187d9eb5420bb
Arun kumar rout <157638000+Arun-kumar-rout12@users.noreply.github.com>
2025-02-07 09:41:31 +0530

    Delete apps.py

--- apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class MyAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Social'
-
-    def ready(self):
-        import Social.signals  
