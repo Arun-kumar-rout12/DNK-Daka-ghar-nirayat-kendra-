9aced2ce94881dfe12077c72602aaa1ad1cd8eec
Arun kumar rout <157638000+Arun-kumar-rout12@users.noreply.github.com>
2025-02-08 11:01:08 +0530

    Update settings.py

--- My_face_clone/settings.py
+++ My_face_clone/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+PORT = os.getenv("PORT", "8000")  # Default to 8000 if PORT is not set
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
